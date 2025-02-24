65ee338f8794976266d2c9d64eec2f501e50c183
Shantanu Atmakuri <shaan2k3@gmail.com>
2025-02-24 15:59:24 +0530

    Built Cart, Line Items
    Built cart, Linked line_items with cart and products, added new column "quantity" to line_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_22_140015) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_24_102304) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "cart_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "image_url"
-    t.decimal "price", precision: 8, scale: 2
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
 end
